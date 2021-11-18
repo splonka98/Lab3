39f45a1e322de255b3338999602761126d2ac67b
splonka98 <szymon.plonka@microsoft.wsei.edu.pl>
2021-11-18 18:42:04 +0100

    initial commit

---
+++ Lab3/Controllers/Lab3Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class Lab3Controller : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FormModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("Created", model);
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ Lab3/Models/FormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class FormModel
+    {
+        public string Name { get; set; }
+
+        
+       [Range(1,5)]
+        public int Repeats { get; set; }
+    }
+}
